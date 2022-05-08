340ef6c512700ce3bd5a189d1b26dab9ca9f1b4c
AfshaBanu <afshabanu9845@gmail.com>
2022-05-08 15:25:01 +0530

    to check the anagram or not

---
+++ pallindrome/src/anagram.java
@@ -0,0 +1,35 @@
+// To check whether the strings are anagram or not
+import java.util.Arrays;
+import java.util.Scanner;
+public class anagram 
+{
+
+		   public static void main(String[] args) 
+		 {
+	      Scanner sc=new Scanner(System.in);
+	      System.out.println("enter the first string");
+	      String str1=sc.next();
+	      System.out.println("enter the second string");
+	      String str2=sc.next();
+
+	      str1=str1.toLowerCase();
+	      str2=str2.toLowerCase();
+
+	      char[] charArray1=str1.toCharArray();
+	      char[] charArray2=str2.toCharArray();
+
+	      Arrays.sort(charArray1);
+	      Arrays.sort(charArray2);
+
+	      boolean result=Arrays.equals(charArray1,charArray2);
+
+	      if(result)
+	    	  System.out.println(str1 +" and "+ str2 +" are anagram");
+	      else
+	    	System.out.println(str1 +" and "+ str2 +" are not an anagram");  
+
+		}
+
+	}
+
+
